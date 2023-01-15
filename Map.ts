0c86edf35f05fa00fe12d5f99305e0844b8855e1
KirillMar <re4r@bk.ru>
2023-01-15 21:11:46 +0300

    fourteen

--- Map.ts
+++ Map.ts
@@ -1,7 +1,8 @@
-function map(array: unknown[], f: (item: unknown) => unknown): unknown[] {
+function map<A, B>(array: A[], f: (item: A) => B): B[] {
     let result = [];
     for (let i = 0; i < array.length; i++) {
         result[i] = f(array[i]);
     }
     return result;
 }
+
