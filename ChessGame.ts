568f863c98338ed50dd7550934e74d70c29e390e
KirillMar <re4r@bk.ru>
2023-01-16 15:31:10 +0300

    eightteen

--- ChessGame.ts
+++ ChessGame.ts
@@ -1,6 +1,22 @@
 import { type } from "os";
 
-class Game {};
+class Game {
+    private pieces = Game.makePieces();
+    private static makePieces() {
+        return [
+            new King('White', 'E', 1),
+            new King('White', 'E', 8),
+
+            new Queen('White', 'D', 1),
+            new Queen('Black', 'D', 8),
+
+            new Bishop('White', 'C', 1),
+            new Bishop('White', 'F', 1),
+            new Bishop('Black', 'C', 8),
+            new Bishop('Black', 'F', 8)
+        ];
+    }
+};
 
 abstract class Piece {
     protected position: Position;
