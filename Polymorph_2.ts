5de43c5da3c1653c4a69ac06082a27faf02b94a1
KirillMar <re4r@bk.ru>
2023-01-16 14:35:19 +0300

    seventeen

--- Polymorph_2.ts
+++ Polymorph_2.ts
@@ -1,4 +1,4 @@
-function call(f: (...args: unknown[]) => unknown, ...args: unknown[]): unknown {
+function call<T extends unknown[], R>(f: (...args: T) => R, ...args: T): R {
     return f(...args);
 };
 
@@ -6,3 +6,4 @@ function fill(length: number, value: string): string[] {
     return Array.from({length}, () => value);
 };
 
+call(fill, 10, 'a');
