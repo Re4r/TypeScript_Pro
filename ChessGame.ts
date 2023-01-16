22a0c6efc99400e2dd6248eaea7e6b552bd4b9a6
KirillMar <re4r@bk.ru>
2023-01-16 15:19:08 +0300

    eightteen

--- ChessGame.ts
+++ ChessGame.ts
@@ -2,13 +2,15 @@ import { type } from "os";
 
 class Game {};
 
-class Piece {
+abstract class Piece {
     protected position: Position;
     constructor(
         private readonly color: Color,
         file: File,
         rank: Rank
     ) {this.position = new Position(file, rank)};
+    public moveTo(position: Position) {this.position = position};
+    abstract canMoveTo(position: Position): boolean;
 };
 
 class Position {
