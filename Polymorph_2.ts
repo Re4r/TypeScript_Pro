4509200e89ab3c83b641bcb99fa3ed266941806f
KirillMar <re4r@bk.ru>
2023-01-16 14:31:57 +0300

    seventeen

--- Polymorph_2.ts
+++ Polymorph_2.ts
@@ -2,3 +2,7 @@ function call(f: (...args: unknown[]) => unknown, ...args: unknown[]): unknown {
     return f(...args);
 };
 
+function fill(length: number, value: string): string[] {
+    return Array.from({length}, () => value);
+};
+
