f20b6764064cf22c033dab3914ce35f1bfab0b4b
KirillMar <re4r@bk.ru>
2023-01-16 14:25:38 +0300

    sixteen

--- Polymorph.ts
+++ Polymorph.ts
@@ -7,3 +7,8 @@ function logPerimeter<Shape extends HasSides & SidesHaveLength>(s: Shape): Shape
     console.log(s.numberOfSides * s.sideLength);
     return s;
 };
+
+type Square = HasSides & SidesHaveLength;
+let squre: Square = {numberOfSides: 4, sideLength: 3};
+
+logPerimeter(squre);
