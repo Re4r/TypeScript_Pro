5a686ff4b7e6aaffd6f74e15b1ff6f7c04808b75
KirillMar <re4r@bk.ru>
2023-01-16 15:23:22 +0300

    eightteen

--- ChessGame.ts
+++ ChessGame.ts
@@ -17,7 +17,13 @@ class Position {
     constructor(
         private file: File,
         private rank: Rank
-    ) {}
+    ) {};
+    distanceFrom(position: Position) {
+        return {
+            rank: Math.abs(position.rank - this.rank),
+            file: Math.abs(position.file.charCodeAt(0) - this.file.charCodeAt(0))
+        };
+    };
 };
 
 class King extends Piece {};
