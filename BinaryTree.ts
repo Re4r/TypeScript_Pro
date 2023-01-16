561c5edb4aa6531b7649bab022b6eb63ec4663e3
KirillMar <re4r@bk.ru>
2023-01-16 14:06:47 +0300

    fiveteen

--- BinaryTree.ts
+++ BinaryTree.ts
@@ -1,7 +1,13 @@
 import { type } from "os";
 
 // Объявление типов для узлов
+// Уровень типов
 type TreeNode = {value: string};
 type LeafNode = TreeNode & {isLeaf: true};
 type InnerNode = TreeNode & {children: [TreeNode] | [TreeNode, TreeNode]};
+
+// Уровень значений
+let a: TreeNode = {value: 'a'};
+let b: LeafNode = {value: 'b', isLeaf: true};
+let c: InnerNode = {value: 'c', children: [b]};
 
