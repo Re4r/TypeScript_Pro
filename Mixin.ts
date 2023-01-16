cad326049254fa0896c0f15ebcbd65a664990450
KirillMar <re4r@bk.ru>
2023-01-16 19:53:59 +0300

    twentyTwo

--- Mixin.ts
+++ Mixin.ts
@@ -4,3 +4,10 @@ class User {}
 
 type ClassConstructor = new(...args: any[]) => {}
 
+function withEZDebug<C extends ClassConstructor>(Class: C) {
+    return class extends Class {
+        constructor(...args: any[]) {
+            super(...args)
+        }
+    }
+}
