43bc4f00cff89b53f5396b20962039d2eb08a3e5
KirillMar <re4r@bk.ru>
2023-01-16 15:41:41 +0300

    nineteen

--- Set.ts
+++ Set.ts
@@ -2,3 +2,9 @@ let set = new Set;
 set.add(1).add(2).add(3);
 console.log(set.has(1));
 console.log(set.has(4));
+
+class Set1 {
+    has(value: number): boolean {};
+    add(value: number): this {};
+    delete(value: number): boolean {};
+} 
